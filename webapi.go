f513a7ae50b831efe9a78120f85d70d789fa311b
Hannes Lohmander <hannes.lohmander@starrepublic.com>
2015-12-01 16:57:15 -0500

    Add godocs to webapi.go

--- webapi.go
+++ webapi.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// HTTP methods
 const (
 	GET    = "GET"
 	POST   = "POST"
@@ -12,30 +13,51 @@ const (
 	DELETE = "DELETE"
 )
 
+// Handler function type used to handle HTTP requests
 type Handler func(*Request) (int, Response)
+
+// Middleware any function that takes and returns a handler
 type Middleware func(Handler) Handler
 
+// GetSupported is the interface that a resource has
+// to implement in order to receive GET HTTP requests
 type GetSupported interface {
 	Get(*Request) (int, Response)
 }
 
+// PostSupported is the interface that a resource has
+// to implement in order to receive POST HTTP requests
 type PostSupported interface {
 	Post(*Request) (int, Response)
 }
 
+// PutSupported is the interface that a resource has
+// to implement in order to receive PUT HTTP requests
 type PutSupported interface {
 	Put(*Request) (int, Response)
 }
 
+// DeleteSupported is the interface that a resource has
+// to implement in order to receive DELETE HTTP requests
 type DeleteSupported interface {
 	Delete(*Request) (int, Response)
 }
 
+// An API manages the resource endpoints that has been added
+// to it and routes requests to the appropriate handler function(s).
+//
+// Since WebApi implements the net/http Handler interface, you can
+// add any number of APIs under the same port/server, prefix your
+// API and so on. For instance
+//
+// 	http.Handle("/api/v1", apiv1)
+// 	http.Handle("/api/v2", apiv2)
 type WebApi struct {
 	mux        *Mux
 	middleware []Middleware
 }
 
+// NewAPI returns a new instance of WebApi
 func NewAPI() *WebApi {
 	return &WebApi{&Mux{}, nil}
 }
@@ -117,14 +139,17 @@ func (webApi *WebApi) requestHandler(resource interface{}, middleware ...Middlew
 	}
 }
 
+// Apply applies middleware to all subsequently added resources
 func (webApi *WebApi) Apply(middleware ...Middleware) {
 	webApi.middleware = middleware
 }
 
+// Add adds a resource to the API
 func (webApi *WebApi) Add(path string, resource interface{}, middleware ...Middleware) {
 	webApi.mux.HandleFunc(path, webApi.requestHandler(resource, middleware...))
 }
 
+// ServeHTTP aliases the webApi.mux.ServeHTTP function
 func (webApi *WebApi) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	webApi.mux.ServeHTTP(rw, r)
 }
